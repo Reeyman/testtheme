05fa20bafa252300be393d6604fe220012bde451
Reeyman <47076219+Reeyman@users.noreply.github.com>
2024-11-11 23:45:54 +0100

    Add files via upload

---
+++ build/webpack.config.js
@@ -0,0 +1,15 @@
+const { join, resolve } = require('path'); 
+module.exports = () => { 
+    return { 
+        resolve: { 
+            alias: {
+                '@swiper': resolve( 
+                    join(__dirname, '..', 'node_modules', 'swiper') 
+                ),
+                '@swipermodules': resolve( 
+                    join(__dirname, '..', 'node_modules', 'swiper/modules/index.mjs') 
+                ),
+            } 
+        } 
+    }; 
+}
